94a6be5134d21ea7081244914cd28cf2221599f2
ignat99 <ignat99@gmail.com>
2018-10-02 16:28:21 +0300

    PyTest 2

--- test_a_proxy.py
+++ test_a_proxy.py
@@ -3,9 +3,8 @@
 """
 Test jumping drom proxy to proxy.
 """
-
-import pytest
 import socket as s
+import pytest
 from a_proxy import Synchronizer
 
 #@pytest.fixture
@@ -16,25 +15,31 @@ from a_proxy import Synchronizer
 #    request.addfinalizer(socket_teardown)
 #    return _socket
 
+#def test_server_connect(socket):
+#    socket.connect(('localhost', 8080))
+#    assert socket
+
 @pytest.yield_fixture
-def socket():
+def socket1():
+    """Open and close socket"""
     _socket = s.socket(s.AF_INET, s.SOCK_STREAM)
     yield _socket
     _socket.close()
 
 @pytest.fixture(scope='module')
-def Server():
-    class Dummy:
+def dummy_server():
+    """Prepare object Host"""
+    class Dummy(object): # pylint: disable=too-few-public-methods
+        """Just any port for test"""
         host_port = 'localhost', 8080
         uri = 'http://%s:%s/' % host_port
+
     return Dummy
 
-#def test_server_connect(socket):
-#    socket.connect(('localhost', 8080))
-#    assert socket
 
-def test_server_connect(socket, Server):
-    socket.connect(Server.host_port)
+def test_server_connect(socket, dummy_server):
+    """Test open port"""
+    socket.connect(dummy_server.host_port)
     assert socket
 
 def test_my_add():
@@ -42,15 +47,3 @@ def test_my_add():
     test_s = Synchronizer()
     test_s.my_add(1111, ('192.168.0.1', 2222))
     assert test_s.forwarders[1111] == ('192.168.0.1', 2222)
-
-
-
-
-#while True:
-#S.my_run()
-#S.my_add(1111, ('192.168.0.1', 2222))
-
-#test_my_add()
-#test_server_connect()
-
-#ForwarderServer(('192.168.0.171', 433), 433)
