3550eee911025eaef264fdf567c917f9c1906ad7
ignat99 <ignat99@gmail.com>
2018-10-02 16:01:12 +0300

    PyTest 1

--- test_a_proxy.py
+++ test_a_proxy.py
@@ -4,11 +4,39 @@
 Test jumping drom proxy to proxy.
 """
 
-#import pytest
+import pytest
+import socket as s
 from a_proxy import Synchronizer
 
+#@pytest.fixture
+#def socket(request):
+#    _socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+#    def socket_teardown():
+#        _socket.close()
+#    request.addfinalizer(socket_teardown)
+#    return _socket
 
-#@pytest.fixture()
+@pytest.yield_fixture
+def socket():
+    _socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+    yield _socket
+    _socket.close()
+
+@pytest.fixture(scope='module')
+def Server():
+    class Dummy:
+        host_port = 'localhost', 8080
+        uri = 'http://%s:%s/' % host_port
+    return Dummy
+
+#def test_server_connect(socket):
+#    socket.connect(('localhost', 8080))
+#    assert socket
+
+def test_server_connect(socket, Server):
+    socket.connect(Server.host_port)
+    assert socket
+
 def test_my_add():
     """Unit Test of list"""
     test_s = Synchronizer()
@@ -22,6 +50,7 @@ def test_my_add():
 #S.my_run()
 #S.my_add(1111, ('192.168.0.1', 2222))
 
-test_my_add()
+#test_my_add()
+#test_server_connect()
 
 #ForwarderServer(('192.168.0.171', 433), 433)
