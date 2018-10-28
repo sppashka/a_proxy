602c7117c2fb36c39901eca57d98a617da5988c5
ignat99 <ignat99@gmail.com>
2018-10-28 19:07:54 +0300

    Pytest 19

--- test_a_proxy.py
+++ test_a_proxy.py
@@ -4,8 +4,12 @@
 Test jumping drom proxy to proxy.
 """
 import socket as s
+from collections import namedtuple
 import pytest
 from a_proxy import Synchronizer
+
+Srv = namedtuple('dummy_server', 'host port')
+REAL_IP = s.gethostbyname(s.gethostname())
 
 #@pytest.fixture
 #def socket(request):
@@ -19,6 +23,10 @@ from a_proxy import Synchronizer
 #    socket.connect(('localhost', 8080))
 #    assert socket
 
+def idparametrize(name, values, fixture=False):
+    """Auxiliary decorator for parametrize"""
+    return pytest.mark.parametrize(name, values, ids=map(repr, values), indirect=fixture)
+
 @pytest.yield_fixture
 def socket():
     """Open and close socket"""
@@ -27,28 +35,41 @@ def socket():
     _socket.close()
 
 
+#@pytest.fixture(scope='module')
+#def dummy_server():
+#    """Prepare object Host then live full time of runing that module"""
+#    class Dummy(object): # pylint: disable=too-few-public-methods
+#        """Just any port for test"""
+#        host_port = 'localhost', 8080
+#        uri = 'http://%s:%s/' % host_port
+#
+#    return Dummy
+
+
 @pytest.fixture(scope='module')
-def dummy_server():
+def dummy_server(request):
     """Prepare object Host then live full time of runing that module"""
     class Dummy(object): # pylint: disable=too-few-public-methods
         """Just any port for test"""
-        host_port = 'localhost', 8080
-        uri = 'http://%s:%s/' % host_port
+        def __init__(self, srv):
+            self.srv = srv
 
-    return Dummy
-
-
-@pytest.fixture(scope='module')
-def dummy2_server():
-    """Prepare object Host then live full time of runing that module"""
-    class Dummy2(object): # pylint: disable=too-few-public-methods
-        """Just any port for test"""
-        host_port = 'localhost', 1111
-        uri = 'http://%s:%s/' % host_port
-
-    return Dummy2
+        @property
+        def host_port(self):
+            """Just any port for test from parameters"""
+            my_str = '{host} {port}'.format(**self.srv._asdict()).split()
+            return (my_str[0], int(my_str[1]))
 
 
+        @property
+        def uri(self):
+            """Just any port for test from parameters"""
+            return 'http://{host}:{port}/'.format(**self.srv._asdict())
+
+    return Dummy(request.param)
+
+
+@idparametrize('dummy_server', [Srv('localhost', 8080), Srv(REAL_IP, 8080)], fixture=True)
 def test_server_connect(socket, dummy_server): # pylint: disable=redefined-outer-name
     """Test open port"""
     socket.connect(dummy_server.host_port)
@@ -60,12 +81,13 @@ def test_my_add():
     test_s.my_add(1111, ('192.168.0.1', 2222))
     assert test_s.forwarders[1111] == ('192.168.0.1', 2222)
 
-def test_my_forward(socket, dummy2_server): # pylint: disable=redefined-outer-name
+@idparametrize('dummy_server', [Srv('localhost', 1111), Srv(REAL_IP, 1111)], fixture=True)
+def test_my_forward(socket, dummy_server): # pylint: disable=redefined-outer-name
     """Test open port 1111"""
     test_s = Synchronizer()
     test_s.go_forward = False
     test_s.my_forward(1111, ('127.0.0.1', 4444))
-    socket.connect(dummy2_server.host_port)
+    socket.connect(dummy_server.host_port)
     assert socket
     test_s.my_remove(1111)
 
